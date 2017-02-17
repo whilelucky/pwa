2300af0febedcfee5371c04ee65302dbd2238da2
Lakshya Ranganath <lakshya.ranganath@gmail.com>
2017-02-18 00:01:02 +0530

    clean webpack configs

--- webpack.client.babel.js
+++ webpack.client.babel.js
@@ -1,6 +1,5 @@
-/* eslint-disable global-require, max-len */
+/* eslint-disable max-len */
 import webpack from 'webpack';
-import merge from 'webpack-merge';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import AssetsPlugin from 'assets-webpack-plugin';
 import SWPrecacheWebpackPlugin from 'sw-precache-webpack-plugin';
@@ -10,7 +9,12 @@ import path from 'path';
 const __NODE_ENV__ = process.env.NODE_ENV;
 const __PWA_ENV__ = process.env.PWA_ENV;
 
-const commonConfig = {
+const ifProd = (value, alternate) =>
+  (__NODE_ENV__ === 'production' ? value : alternate);
+
+const webpackClientConfig = {
+  cache: ifProd(false, true),
+
   entry: {
     main: ['./client/index.js'],
     vendor: ['./client/vendor.js'],
@@ -18,11 +22,33 @@ const commonConfig = {
 
   output: {
     path: path.resolve('./build/client'),
+    filename: ifProd('js/[name].[chunkhash:8].js', 'js/[name].js'),
+    chunkFilename: ifProd('js/[name].[chunkhash:8].js', 'js/[name].js'),
+  },
+
+  module: {
+    rules: ifProd([
+      { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader?importLoaders=1!postcss-loader' }) },
+      { test: /\.(gif|png|jpe?g|svg|ico)$/i, use: [{ loader: 'file-loader', options: { name: 'assets/images/[name].[hash:8].[ext]' } }] },
+      { test: /\.(woff(2)?|ttf|otf|eot)(\?[a-z0-9=&.]+)?$/, use: [{ loader: 'url-loader', options: { limit: 1000, name: 'assets/fonts/[name].[hash:8].[ext]' } }] },
+    ], [
+      { test: /\.js$/, exclude: /node_modules/, use: [{ loader: 'babel-loader', options: { cacheDirectory: 'babel_cache' } }] },
+      { test: /\.css$/, use: ['style-loader', { loader: 'css-loader', options: { importLoaders: 1 } }, 'postcss-loader'] },
+      { test: /\.(gif|png|jpe?g|svg|ico)$/i, use: [{ loader: 'file-loader', options: { name: 'assets/images/[name].[ext]' } }] },
+      { test: /\.(woff(2)?|ttf|otf|eot)(\?[a-z0-9=&.]+)?$/, use: [{ loader: 'url-loader', options: { limit: 1000, name: 'assets/fonts/[name].[ext]' } }] },
+    ]),
   },
 
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': ifProd('"production"', '"development"'),
+      __BROWSER__: true,
+      __PWA_ENV__: JSON.stringify(__PWA_ENV__),
+      __LOCAL__: __PWA_ENV__ === 'local',
+    }),
     new webpack.optimize.CommonsChunkPlugin({
       names: ['vendor', 'manifest'],
       minChunks: Infinity,
@@ -32,95 +58,42 @@ const commonConfig = {
       path: path.resolve('./build/client'),
       prettyPrint: true,
     }),
-  ],
-};
-
-const productionConfig = {
-  output: {
-    filename: 'js/[name].[chunkhash:8].js',
-    chunkFilename: 'js/[name].[chunkhash:8].js',
-  },
-
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader?importLoaders=1!postcss-loader' }) },
-      { test: /\.(gif|png|jpe?g|svg|ico)$/i, use: [{ loader: 'file-loader', options: { name: 'assets/images/[name].[hash:8].[ext]' } }] },
-      { test: /\.(woff(2)?|ttf|otf|eot)(\?[a-z0-9=&.]+)?$/, use: [{ loader: 'url-loader', options: { limit: 1000, name: 'assets/fonts/[name].[hash:8].[ext]' } }] },
-    ],
-  },
-
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"',
-      __BROWSER__: true,
-      __PWA_ENV__: JSON.stringify(__PWA_ENV__),
-      __LOCAL__: __PWA_ENV__ === 'local',
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-    }),
-    new webpack.HashedModuleIdsPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        screw_ie8: true,
-        warnings: false,
-      },
-      mangle: {
-        screw_ie8: true,
-      },
-      output: {
-        comments: false,
-        screw_ie8: true,
-      },
-    }),
-    new ExtractTextPlugin({
-      filename: 'assets/css/[name].[contenthash:8].css',
-      allChunks: true,
-    }),
-    new SWPrecacheWebpackPlugin({
-      cacheId: 'pwa',
-      filename: 'js/serviceWorker.js',
-      minify: true,
-    }),
+    ...ifProd([
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+      }),
+      new webpack.HashedModuleIdsPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+        compress: {
+          screw_ie8: true,
+          warnings: false,
+        },
+        mangle: {
+          screw_ie8: true,
+        },
+        output: {
+          comments: false,
+          screw_ie8: true,
+        },
+      }),
+      new ExtractTextPlugin({
+        filename: 'assets/css/[name].[contenthash:8].css',
+        allChunks: true,
+      }),
+      new SWPrecacheWebpackPlugin({
+        cacheId: 'pwa',
+        filename: 'js/serviceWorker.js',
+        minify: true,
+      }),
+    ], [
+      new webpack.NamedModulesPlugin(),
+      new DashboardPlugin(),
+    ]),
   ],
 
-  devtool: 'hidden-source-map',
+  devtool: ifProd('hidden-source-map', 'inline-source-map'),
 };
-
-const developmentConfig = {
-  output: {
-    filename: 'js/[name].js',
-    chunkFilename: 'js/[name].js',
-  },
-
-  module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, use: [{ loader: 'babel-loader', options: { cacheDirectory: 'babel_cache' } }] },
-      { test: /\.css$/, use: ['style-loader', { loader: 'css-loader', options: { importLoaders: 1 } }, 'postcss-loader'] },
-      { test: /\.(gif|png|jpe?g|svg|ico)$/i, use: [{ loader: 'file-loader', options: { name: 'assets/images/[name].[ext]' } }] },
-      { test: /\.(woff(2)?|ttf|otf|eot)(\?[a-z0-9=&.]+)?$/, use: [{ loader: 'url-loader', options: { limit: 1000, name: 'assets/fonts/[name].[ext]' } }] },
-    ],
-  },
-
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"development"',
-      __BROWSER__: true,
-      __PWA_ENV__: JSON.stringify(__PWA_ENV__),
-      __LOCAL__: __PWA_ENV__ === 'local',
-    }),
-    new webpack.NamedModulesPlugin(),
-    new DashboardPlugin(),
-  ],
-
-  devtool: 'inline-source-map',
-};
-
-const webpackClientConfig = __NODE_ENV__ === 'production'
-  ? merge.smart(commonConfig, productionConfig)
-  : merge.smart(commonConfig, developmentConfig);
 
 if (__PWA_ENV__ === 'local') {
   webpackClientConfig.output.publicPath = 'http://localhost:8080/build/client/';
--- webpack.server.babel.js
+++ webpack.server.babel.js
@@ -1,12 +1,14 @@
-/* eslint-disable global-require, max-len */
+/* eslint-disable max-len */
 import webpack from 'webpack';
-import merge from 'webpack-merge';
 import nodeExternals from 'webpack-node-externals';
 
 const __NODE_ENV__ = process.env.NODE_ENV;
 const __PWA_ENV__ = process.env.PWA_ENV;
 
-const commonConfig = {
+const ifProd = (value, alternate) =>
+  (__NODE_ENV__ === 'production' ? value : alternate);
+
+const webpackServerConfig = {
   entry: './server/index.js',
 
   target: 'node',
@@ -17,86 +19,61 @@ const commonConfig = {
   ],
 
   output: {
+    path: './build/server',
     filename: 'index.js',
     chunkFilename: '[name].js',
-    path: './build/server',
     libraryTarget: 'commonjs',
   },
 
   module: {
-    loaders: [
+    rules: ifProd([
       { test: /\.css$/, use: ['isomorphic-style-loader', 'css-loader'] },
-    ],
+      { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },
+      { test: /\.(gif|png|jpe?g|svg|ico)$/i, use: [{ loader: 'file-loader', options: { name: 'assets/images/[name].[hash:8].[ext]' } }] },
+      { test: /\.(woff(2)?|ttf|otf|eot)(\?[a-z0-9=&.]+)?$/, use: [{ loader: 'url-loader', options: { limit: 1000, name: 'assets/fonts/[name].[hash:8].[ext]' } }] },
+    ], [
+      { test: /\.css$/, use: ['isomorphic-style-loader', 'css-loader'] },
+      { test: /\.js$/, exclude: /node_modules/, use: [{ loader: 'babel-loader', options: { cacheDirectory: 'babel_cache' } }] },
+      { test: /\.(gif|png|jpe?g|svg|ico)$/i, use: [{ loader: 'file-loader', options: { name: 'assets/images/[name].[ext]' } }] },
+      { test: /\.(woff(2)?|ttf|otf|eot)(\?[a-z0-9=&.]+)?$/, use: [{ loader: 'url-loader', options: { limit: 1000, name: 'assets/fonts/[name].[ext]' } }] },
+    ]),
   },
 
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': ifProd('"production"', '"development"'),
+      __BROWSER__: false,
+      __PWA_ENV__: JSON.stringify(__PWA_ENV__),
+      __LOCAL__: __PWA_ENV__ === 'local',
+    }),
+    ...ifProd([
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+      }),
+      new webpack.HashedModuleIdsPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+        compress: {
+          screw_ie8: true,
+          warnings: false,
+        },
+        mangle: {
+          screw_ie8: true,
+        },
+        output: {
+          comments: false,
+          screw_ie8: true,
+        },
+      }),
+    ], [
+      new webpack.NamedModulesPlugin(),
+    ]),
   ],
 
   devtool: 'source-map',
 };
-
-const productionConfig = {
-  module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },
-      { test: /\.(gif|png|jpe?g|svg|ico)$/i, use: [{ loader: 'file-loader', options: { name: 'assets/images/[name].[hash:8].[ext]' } }] },
-      { test: /\.(woff(2)?|ttf|otf|eot)(\?[a-z0-9=&.]+)?$/, use: [{ loader: 'url-loader', options: { limit: 1000, name: 'assets/fonts/[name].[hash:8].[ext]' } }] },
-    ],
-  },
-
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"',
-      __BROWSER__: false,
-      __PWA_ENV__: JSON.stringify(__PWA_ENV__),
-      __LOCAL__: __PWA_ENV__ === 'local',
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-    }),
-    new webpack.HashedModuleIdsPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        screw_ie8: true,
-        warnings: false,
-      },
-      mangle: {
-        screw_ie8: true,
-      },
-      output: {
-        comments: false,
-        screw_ie8: true,
-      },
-    }),
-  ],
-};
-
-const developmentConfig = {
-  module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, use: [{ loader: 'babel-loader', options: { cacheDirectory: 'babel_cache' } }] },
-      { test: /\.(gif|png|jpe?g|svg|ico)$/i, use: [{ loader: 'file-loader', options: { name: 'assets/images/[name].[ext]' } }] },
-      { test: /\.(woff(2)?|ttf|otf|eot)(\?[a-z0-9=&.]+)?$/, use: [{ loader: 'url-loader', options: { limit: 1000, name: 'assets/fonts/[name].[ext]' } }] },
-    ],
-  },
-
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"development"',
-      __BROWSER__: false,
-      __PWA_ENV__: JSON.stringify(__PWA_ENV__),
-      __LOCAL__: __PWA_ENV__ === 'local',
-    }),
-    new webpack.NamedModulesPlugin(),
-  ],
-};
-
-const webpackServerConfig = __NODE_ENV__ === 'production'
-  ? merge.smart(commonConfig, productionConfig)
-  : merge.smart(commonConfig, developmentConfig);
 
 if (__PWA_ENV__ === 'local') {
   webpackServerConfig.output.publicPath = 'http://localhost:8080/build/client/';
