405b55e6ce0be2e5ee7bfa843cc337cfd94127a4
Lakshya Ranganath <lakshya.ranganath@gmail.com>
2017-02-28 03:49:42 +0530

    add helmet, cleanup server bootstrap

--- server/index.js
+++ server/index.js
@@ -1,23 +1,26 @@
 import 'babel-polyfill';
 import express from 'express';
+import helmet from 'helmet';
 import compression from 'compression';
 import morgan from 'morgan';
 import slashes from 'connect-slashes';
 import reactMiddleware from './middlewares/reactMiddleware';
 
 const PORT = process.env.PORT || 8000;
+
 const app = express();
 
-app
-  .set('trust proxy', true)
-  .use(compression())
-  .use(morgan(__LOCAL__ ? 'dev' : 'combined'))
-  .use('/build/client', express.static('build/client'))
-  .use('/serviceWorker.js', express.static('build/client/js/serviceWorker.js'))
-  .use('/manifest.json', express.static('build/client/manifest.json'))
-  .use(slashes(true))
-  .use(reactMiddleware)
-  .listen(PORT, () => {
-    // eslint-disable-next-line
-    console.info(`pwa is running as ${__PWA_ENV__} on port ${PORT}`);
-  });
+app.set('trust proxy', true);
+app.use(helmet({ dnsPrefetchControl: false }));
+app.use(compression());
+app.use(morgan(__LOCAL__ ? 'dev' : 'combined'));
+app.use('/build/client', express.static('build/client'));
+app.use('/serviceWorker.js', express.static('build/client/js/serviceWorker.js'));
+app.use('/manifest.json', express.static('build/client/manifest.json'));
+app.use(slashes(true));
+app.use(reactMiddleware);
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line
+  console.info(`pwa is running as ${__PWA_ENV__} on port ${PORT}`);
+});
