a796f39235c64cda9e221f3977e07a24d2613904
Lakshya Ranganath <lakshya.ranganath@gmail.com>
2017-03-29 12:29:48 +0530

    synchronously load route-chunks on server

--- client/routes/index.js
+++ client/routes/index.js
@@ -1,19 +1,25 @@
-/* eslint-disable import/first, import/extensions, import/no-extraneous-dependencies, import/no-unresolved, import/no-webpack-loader-syntax, max-len */
+/* eslint-disable global-require, import/no-extraneous-dependencies, import/no-unresolved, import/no-webpack-loader-syntax, max-len */
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Wrapper from '../containers/Wrapper/Wrapper';
 import NotFound from '../components/NotFound/NotFound';
-import LandingPage from 'bundle-loader?lazy&name=Home!../containers/LandingPage/LandingPage';
 
-const load = (bundle) => (nextState, cb) =>
-  bundle((module) => cb(null, module.default));
+const load = (chunk) => (nextState, cb) => (
+  __BROWSER__
+    ? chunk((lazyChunk) => cb(null, lazyChunk.default))
+    : cb(null, chunk.default)
+);
 
 export default (
   <Route path="/" component={Wrapper}>
 
     <IndexRoute
       name="landing"
-      getComponent={load(LandingPage)}
+      getComponent={
+        __BROWSER__
+          ? load(require('bundle-loader?lazy&name=Home!../containers/LandingPage/LandingPage'))
+          : load(require('../containers/LandingPage/LandingPage'))
+      }
     />
 
     <Route path="*" component={NotFound} />
