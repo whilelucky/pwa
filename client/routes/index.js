8babbd7940959287c2bda03aebaa9c1eb46c2a89
Lakshya Ranganath <lakshya.ranganath@gmail.com>
2017-04-20 17:31:39 +0530

    lazy load route-split chunks

--- client/routes/index.js
+++ client/routes/index.js
@@ -3,18 +3,16 @@ import { Route, IndexRoute } from 'react-router';
 import Wrapper from '../containers/Wrapper/Wrapper';
 import NotFound from '../components/NotFound/NotFound';
 
-const load = (chunkPromise) => (_, cb) =>
-  chunkPromise
-    .then((module) => module.default)
-    .then((page) => cb(null, page))
-    .catch((error) => cb(error, null));
-
 export default (
   <Route path="/" component={Wrapper}>
 
     <IndexRoute
       name="landing"
-      getComponent={load(import('../containers/LandingPage/LandingPage' /* webpackChunkName: 'landing' */))}
+      getComponent={(_, cb) =>
+        import('../containers/LandingPage/LandingPage' /* webpackChunkName: 'landing' */)
+          .then((module) => cb(null, module.default))
+          .catch((error) => cb(error, null))
+      }
     />
 
     <Route path="*" component={NotFound} />
