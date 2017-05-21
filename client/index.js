0726fd6c464bfdee2e7e65a1098f4295369c0079
Lakshya Ranganath <lakshya.ranganath@gmail.com>
2017-05-22 02:36:36 +0530

    inline Root component

--- client/index.js
+++ client/index.js
@@ -1,13 +1,23 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import { ReduxAsyncConnect } from 'redux-connect';
 import configureStore from './redux/configureStore';
 import './assets/css/core.css';
-import Root from './components/Root/Root';
+import routes from './routes';
 
 const store = configureStore(window.__INITIAL_STATE__);
 
 ReactDOM.render(
-  <Root store={store} />,
+  <Provider store={store} key="provider">
+    <Router
+      routes={routes}
+      history={browserHistory}
+      render={(props) => <ReduxAsyncConnect {...props} />}
+      onUpdate={() => window.scrollTo(0, 0)}
+    />
+  </Provider>,
   document.getElementById('root'),
 );
