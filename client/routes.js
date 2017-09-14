7ce544e7ab060d48cb5cc221c4762455f3da6158
Lakshya Ranganath <lakshya.ranganath@gmail.com>
2017-09-14 21:11:45 +0530

    interdependently download css and js chunks

--- client/routes.js
+++ client/routes.js
@@ -4,20 +4,17 @@ import Wrapper from './views/Wrapper/Wrapper';
 import NotFound from './views/NotFound/NotFound';
 import importCss from './services/importCss';
 
-const loadRoute = (cb) => (module) => cb(null, module.default);
-const failRoute = (cb) => (error) => cb(error, null);
-
 export default (
   <Route path="/" component={Wrapper}>
 
     <IndexRoute
       name="landing"
-      getComponent={
-        (_, cb) => {
-          import('./views/LandingPage/LandingPage' /* webpackChunkName: 'landing' */).then(loadRoute(cb)).catch(failRoute(cb));
-          importCss('landing');
-        }
-      }
+      getComponent={(_, cb) => {
+        Promise.all([
+          import('./views/LandingPage/LandingPage' /* webpackChunkName: 'landing' */),
+          importCss('landing'),
+        ]).then(([module]) => cb(null, module.default));
+      }}
     />
 
     <Route path="*" component={NotFound} />
